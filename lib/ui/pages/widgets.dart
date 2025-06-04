198e328c223c054a4858fa92878960c9007a2809
Janis1310 <janis1310@gmail.com>
2025-06-05 00:27:59 +0200

    creating first standart widgets

---
+++ lib/ui/pages/widgets.dart
@@ -0,0 +1,57 @@
+// widgets.dart
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:vulpack/ui/pages/theme/app_colors.dart';
+
+class CustomBackButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final Color? iconColor;
+  final double? iconSize;
+  final String? tooltip;
+
+  const CustomBackButton({
+    Key? key,
+    this.onPressed,
+    this.iconColor,
+    this.iconSize = 24.0,
+    this.tooltip = 'back',
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: onPressed ?? () => context.pop(),
+      color: iconColor,
+      iconSize: iconSize,
+      tooltip: tooltip,
+    );
+  }
+}
+
+class AddButton extends StatelessWidget {
+  final String route;
+  final String label;
+  final Color backgroundColor;
+
+  const AddButton({
+    super.key,
+    required this.route,
+    this.label = '+ Add',
+    this.backgroundColor = AppColors.primary,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        foregroundColor: Colors.white,
+      ),
+      onPressed: () {
+        context.go(route);
+      },
+      child: Text(label),
+    );
+  }
+}
